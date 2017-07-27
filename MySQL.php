72a8cf273cae21620cece6dab47dea7591a51d89
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-07-27 19:10:03 +0300

    Update MySQL.php

--- MySQL.php
+++ MySQL.php
@@ -28,7 +28,7 @@ class MySQL implements iWorkData
   }
     public function saveData($key, $val)
     {
-    $result = mysql_query("INSERT into MY_TEST VALUES("$key","$val")");
+    $result = mysql_query('INSERT into MY_TEST VALUES("' . $key . '","' . $val . '"))';
 	    echo mysql_error();
 	    if($result){
 	    	echo "Data insertes";
