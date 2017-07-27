5c5e81b8dfcf87ef6bcd542d48943efa315eb9c5
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-07-27 19:15:36 +0300

    Update MySQL.php

--- MySQL.php
+++ MySQL.php
@@ -19,7 +19,7 @@ class MySQL implements iWorkData
     public function dbconnect() {
     $this->conn = mysql_connect($this->host, $this->user, $this->password) or die ("<br/>cant connect to  MySQL");
     if($this->conn){
-    	echo "Connect to DataBase";
+    	echo "Connect to DataBase<br>";
     }
 		
     mysql_select_db($this->dB,$this->conn) or die ("<br/>DB not found");
