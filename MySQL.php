9cc6d40cfed761348f67b8ab6dca12329d0a1c31
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-07-27 18:25:51 +0300

    Update MySQL.php

--- MySQL.php
+++ MySQL.php
@@ -1,6 +1,26 @@
 <?php
 class MySQL implements iWorkData
 {
+    private $user;
+    private $password;
+    private $host;
+    private $dB;
+    private $conn;
+    public function __construct()
+    {
+        $this->user = "user1";
+        $this->dB = "user1";
+        $this->host = "localhost";
+        $this->password = "tuser1";
+    
+    }
+    private function dbconnect() {
+    $this->conn = mysql_connect($this->host, $this->user, $this->password) or die ("<br/>cant connect to  MySQL");
+		
+    mysql_select_db($this->dB,$conn) or die ("<br/>DB not found");
+	
+	return $this->conn;
+  }
     public function saveData($key, $val)
     {
     
