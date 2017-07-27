2b6947a62fc04a748f9cc6dd7b9ae6ece1bf0166
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-07-27 21:00:27 +0300

    Update Session.php

--- Session.php
+++ Session.php
@@ -2,17 +2,29 @@
 class Session implements iWorkData
 {
     public function saveData($key, $val)
-    {
-    
+    {   
+        if(!$key || !$val){
+            return false;
+        }
+        if(isset($key))
+        {
+        $_SESSION[$key] = $val;
+            return true;
+        }
     }
     
     public function getData($key)
     {
+        if(isset($key) || isset($_SESSION[$key]))
+        {
+            return $_SESSION[$key];
+        }
     
     }
     public function deleteData($key)
     {
-    
+        unset($_SESSION[$key]);
+        return true;
     }
 }
 ?>
