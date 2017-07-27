e7e84d133675bf964920dcedc44894067864db58
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-07-27 18:15:42 +0300

    Update iWorkData.php

--- iWorkData.php
+++ iWorkData.php
@@ -1,6 +1,8 @@
- interface iWorkData
+<?php 
+interface iWorkData
     {
         public function saveData($key, $val);
         public function getData($key);
         public function deleteData($key);
     }
+?>
