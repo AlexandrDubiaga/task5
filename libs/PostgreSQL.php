4dcb09d6eed759c9ae1f77309696f6f7d9374d98
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-07-31 17:02:01 +0300

    Update PostgreSQL.php

--- libs/PostgreSQL.php
+++ libs/PostgreSQL.php
@@ -1,6 +1,22 @@
 <?php
 class PostgreSQL implements iWorkData
 {
+    public functionn __construct()
+    {
+        $host = "localhost";
+        $user = "user1";
+        $pass = "user1";
+        $db = "user1";
+        $connection = pg_connect("host=$host dbname=$db user=$user password=$pass");
+        if (!$connection)
+        {
+            die("Could not open connection to database server");
+        }else
+        {
+            echo "Connect";
+        }
+    
+    }
     public function saveData($key, $val)
     {
     
