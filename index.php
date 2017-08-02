a8c426048dd952b27011bf4260920e4b87bf9de7
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-02 18:36:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include('config.php');
 $mysql = new MySQL();
 $mysql->dbconnect();
 $postgre = new PostgreSQL();
-var_dump($arr = $postgre->getData("user10"));
+$arr = $postgre->getData("user10");
 
 
 /*$mysql->saveData("user2","task5");*/
