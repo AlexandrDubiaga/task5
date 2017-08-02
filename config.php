8a9982e5ff2319e0e06055fb92a83c8cf890852d
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-02 19:52:25 +0300

    Update config.php

--- config.php
+++ config.php
@@ -3,4 +3,19 @@ define('NOT_ADD_COOKIE','Cookie not installed<br>');
 define('ADD_COOKIE','Cookie installed<br>');
 define('NOT_DELETE_COOKIE','Cookie not installed<br>');
 define('DELETE_COOKIE','Cookie deleted<br>');
+
+define('ADD_POSTGRE','Postgre added<br>');
+define('DEL_POSTGRE','Postgre deleted<br>');
+define('ADD_MYSQL','Mysql added<br>');
+define('DEL_MYSQL','Mysql deleted<br>');
+
+define('SESSION_SAVE','Session saved<br>');
+define('SESSION_NOT_SAVE','Session not saved<br>');
+define('SESSION_DELETE','Session deleted<br>');
+define('SESSION_NOT_DELETE','Session not deleted<br>');
+
+define('ADD_COOKIE','Cookie saved<br>');
+define('NOT_ADD_COOKIE','Cookie not saved<br>');
+define('DELETE_COOKIE','Delete cookie<br>');
+define('NOT_DELETE_COOKIE','Not delete cookie<br>');
  ?>
