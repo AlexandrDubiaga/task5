29104a089d7ab376713112a0ceadc61edac855b0
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-02 16:52:37 +0300

    Update index.php

--- template/index.php
+++ template/index.php
@@ -5,7 +5,12 @@
           </title>
      </head>
      <body>
+          <h1>Mysql</h1><br>
     <?php
+     foreach($mysqlData as $key => $val)
+     {
+          echo "Key: ".$key," "."Data: ".$val."<br>";
+     }
   
     ?>
      </body>
