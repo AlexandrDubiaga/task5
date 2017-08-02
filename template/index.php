96a828ac9886acc821e2b21a6c1001d2684d34d5
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-02 17:32:22 +0300

    Update index.php

--- template/index.php
+++ template/index.php
@@ -8,15 +8,7 @@
           <h1>Mysql</h1><br>
           
     <?php
-          foreach($mysqlData as  $val)
-          {
-              foreach($val as $key => $item)
-              {
-                    echo $key." ".$item;
-              }
-                echo "<br>";
-          }
-          
+       
    ?>
      </body>
 </html>
