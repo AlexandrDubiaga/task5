7ae562ef21da7f3aa1e4954398b5f4c73a1d86a6
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-02 17:37:43 +0300

    Update MySQL.php

--- libs/MySQL.php
+++ libs/MySQL.php
@@ -35,8 +35,8 @@ class MySQL implements iWorkData
         if (!empty($key)){
             $result = mysql_query("SELECT * FROM MY_TEST WHERE `key`=\"$key\" ") or die("Something wrong with query");
             while($row[]= mysql_fetch_assoc($result)){ }
-        }else echo mysql_error();
-	return $row;
+            }else echo mysql_error();
+	    return $row;
     }
     public function deleteData($key)
     {
