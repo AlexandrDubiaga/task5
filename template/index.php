f48424e5ce0976ab218adedabd1ce19e30721c9a
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-02 19:47:59 +0300

    Update index.php

--- template/index.php
+++ template/index.php
@@ -8,6 +8,17 @@
           <h1>Mysql</h1><br>
           
     <?php
+          echo $postgreAdd;
+          echo $deletePostge;
+          exho $mysqlAdd;
+          echo $mysqlDel;
+          echo $sessionSave;
+          echo $sessionNotSave;
+          echo $sessionDelete;
+          echo $addCookie;
+          echo $notAddCookie;
+          echo $delCookie;
+          echo $notDelCookie;
           foreach($mysqlData as $val)
           {
                echo $val['key'] ." ".$val['data']."<br>";
